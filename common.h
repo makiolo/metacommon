760298e47dc643f50de1283a2ed01a18fb8a6cf4
Ricardo <makiolo@gmail.com>
2019-04-17 17:03:58 +0200

    Update common.h

--- common.h
+++ common.h
@@ -22,6 +22,20 @@
 #endif
 #endif
 
+#if __cplusplus < 201402L
+
+namespace std {
+
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+}
+
+#endif
+
 namespace ctti {
 
 // http://stackoverflow.com/a/15863804
